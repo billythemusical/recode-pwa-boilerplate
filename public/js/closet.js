1c9d837671a55708e5f1b8d68159a4231c82b80f
Glitch (joeyklee-todo-app-mongodb) <none>
2022-01-21 18:20:14 +0000

    🚙💳 Checkpoint ./public/js/closet.js:13079275/1393 ./index.js:13079275/22

--- public/js/closet.js
+++ public/js/closet.js
@@ -3,48 +3,48 @@ const closet = () => {
     let clothes = [
         {
             name: "beanie",
-            img: loadImage("public/images/beanie.png")
+            img: loadImage("https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/beanie.png?v=1642731833951")
         },
-        // {
-        //     name: "body",
-        //     img: loadImage("./images/body.png")
-        // },
-        // {
-        //     name: "jacket",
-        //     img: loadImage("./images/jacket.png")
-        // },
-        // {
-        //     name: "jeans",
-        //     img: loadImage("./images/jeans.png")
-        // },
-        // {
-        //     name: "parka",
-        //     img: loadImage("./images/parka.png")
-        // },
-        // {
-        //     name: "shorts",
-        //     img: loadImage("./images/shorts.png")
-        // },
-        // {
-        //     name: "snow-pants",
-        //     img: loadImage("./images/snow-pants.png")
-        // },
-        // {
-        //     name: "sunglasses",
-        //     img: loadImage("./images/sunglasses.png")
-        // },
-        // {
-        //     name: "tank-top",
-        //     img: loadImage("./images/tank-top.png")
-        // },
-        // {
-        //     name: "tshirt",
-        //     img: loadImage("./images/tshirt.png")
-        // },
-        // {
-        //     name: "umbrella",
-        //     img: loadImage("./images/umbrella.png")
-        // }
+        {
+            name: "body",
+            img: loadImage("https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/body.png?v=1642731834063")
+        },
+        {
+            name: "jacket",
+            img: loadImage("https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/jacket.png?v=1642731834040")
+        },
+        {
+            name: "jeans",
+            img: loadImage("https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/jeans.png?v=1642731834628")
+        },
+        {
+            name: "parka",
+            img: loadImage("https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/parka.png?v=1642731834675")
+        },
+        {
+            name: "shorts",
+            img: loadImage("https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/shorts.png?v=1642731834200")
+        },
+        {
+            name: "snow-pants",
+            img: loadImage("https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/snow-pants.png?v=1642731834707")
+        },
+        {
+            name: "sunglasses",
+            img: loadImage("https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/sunglasses.png?v=1642731834851")
+        },
+        {
+            name: "tank-top",
+            img: loadImage("https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/tank-top.png?v=1642731834978")
+        },
+        {
+            name: "tshirt",
+            img: loadImage("https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/tshirt.png?v=1642731835589")
+        },
+        {
+            name: "umbrella",
+            img: loadImage("https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/umbrella.png?v=1642731835865")
+        }
     ];
 
     return clothes
