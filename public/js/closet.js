85bef82735e9dd42f194593ba9123ed8b8958ba3
Glitch (joeyklee-todo-app-mongodb) <none>
2022-01-21 02:35:54 +0000

    🤗🌚 Checkpoint ./public/js/closet.js:13079275/276

--- public/js/closet.js
+++ public/js/closet.js
@@ -3,47 +3,47 @@ const closet = () => {
     let clothes = [
         {
             name: "beanie",
-            img: loadImage("layout/beanie.png")
+            img: loadImage("images/beanie.png")
         },
         {
             name: "body",
-            img: loadImage("layout/body.png")
+            img: loadImage("images/body.png")
         },
         {
             name: "jacket",
-            img: loadImage("layout/jacket.png")
+            img: loadImage("images/jacket.png")
         },
         {
             name: "jeans",
-            img: loadImage("layout/jeans.png")
+            img: loadImage("images/jeans.png")
         },
         {
             name: "parka",
-            img: loadImage("layout/parka.png")
+            img: loadImage("images/parka.png")
         },
         {
             name: "shorts",
-            img: loadImage("layout/shorts.png")
+            img: loadImage("images/shorts.png")
         },
         {
             name: "snow-pants",
-            img: loadImage("layout/snow-pants.png")
+            img: loadImage("images/snow-pants.png")
         },
         {
             name: "sunglasses",
-            img: loadImage("layout/sunglasses.png")
+            img: loadImage("images/sunglasses.png")
         },
         {
             name: "tank-top",
-            img: loadImage("layout/tank-top.png")
+            img: loadImage("images/tank-top.png")
         },
         {
             name: "tshirt",
-            img: loadImage("layout/tshirt.png")
+            img: loadImage("images/tshirt.png")
         },
         {
             name: "umbrella",
-            img: loadImage("layout/umbrella.png")
+            img: loadImage("images/umbrella.png")
         },
     ];
 
