753934d9af392d7e8f1479d0f87672fbaf95cf8c
Glitch (joeyklee-todo-app-mongodb) <none>
2022-01-23 05:54:19 +0000

    🍣🎊 Checkpoint ./public/js/sw.js:13079275/388

---
+++ sw.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('d4w-store').then((cache) => cache.addAll([
+      '/'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key)
+            return caches.delete(key)
+          }
+        }))
+      })
+      .then(() => self.clients.claim())
+  )
+})
