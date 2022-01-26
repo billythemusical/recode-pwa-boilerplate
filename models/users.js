a399f4e85e94392080bca6954f82230f71e1c244
Glitch (joeyklee-todo-app-mongodb) <none>
2022-01-26 17:08:12 +0000

    👑🌧 Checkpoint ./models/users.js:13079275/519

--- models/users.js
+++ models/users.js
@@ -4,16 +4,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const { defaultCloset } = require('./defaultCloset')
 
-const users = new Schema({
-  [
-    {
-      name: String,
-      email: String,
-      default_set: ["male", "female", "non"],
-      closet: defaultCloset
-    }
-  ]
-});
+const users = new Schema( 
+  { users: [
+      {
+        name: String,
+        email: String,
+        default_set: ["male", "female", "non"],
+        closet: defaultCloset
+      }
+    ]
+  }
+)
 
 const db = mongoose.model('dev-d4w', users)
 
@@ -24,12 +25,14 @@ module.exports = db;
 //   "name": String, // user short name
 //   "email": String, // user email for logging in
 //   "default_set": String, // male, female, non - to deliver a basic set of clothes
-//"closet": [ // the clothes in their closet, everyone starts out with a set
+//   "closet": [ // from defaultCloset.js, the default clothes in their closet based on any gender input, everyone starts out with a set from non?
 //       {
 //           name: String, // "red beanie", user defined?
 //           category: String, // "top", "bottom", "hat", "accessory"
+//           default_set: ["male", "female", "non"],
+//           temp_range: {hi: 0, lo: 0},
 //           img: String, // url to image, should be on server or cdn
-//           temp: { hi: Number, lo: Number }, // to parse for temp, when to show it
+//           temp_range: { hi: Number, lo: Number }, // to parse for temp, when to show it
 //           priority: Number, // from 0 to n, which piece to show in range for multiple, when tapped, it cycles through
 //       }
 //   ]
