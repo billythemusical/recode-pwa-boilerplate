789a000cfd4c4dc4862b6f002f2cd7d338b18511
Glitch (joeyklee-todo-app-mongodb) <none>
2022-01-27 03:49:39 +0000

    📇👻 Checkpoint ./models/users.js:13079275/3784 ./data/defualtCloset.js:13079275/3589 ./index.js:13079275/6

--- models/users.js
+++ models/users.js
@@ -2,15 +2,15 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const { defaultCloset } = require('../js/defaultCloset')
+const { defaultCloset, newClothing } = require('../data/defaultCloset')
 
 const users = new Schema( 
   { users: [
       {
         name: String,
         email: String,
-        default_set: ["male", "female", "non"],
-        closet: defaultCloset
+        default_set: Array,
+        closet: Array
       }
     ]
   }
@@ -18,106 +18,7 @@ const users = new Schema(
 
 const db = mongoose.model('dev-d4w', users)
 
-const defaultCloset = [
-  
-        {
-            name: "beanie",
-            category: "hat",
-            default_set: ["male", "female", "non"],
-            temp_range: {hi: 0, lo: 0},
-            priority: 1,
-            img: "https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/beanie.png?v=1642731833951"
-        },
-        {
-            name: "body",
-            category: "none",
-            default_set: ["male", "female", "non"],
-            temp_range: {hi: 0, lo: 0},
-            priority: 1,
-            img: "https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/body.png?v=1642731834063"
-        },
-        {
-            name: "jacket",
-            category: "top",
-            default_set: ["male", "female", "non"],
-            temp_range: {hi: 0, lo: 0},
-            priority: 1,
-            img: "https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/jacket.png?v=1642731834040"
-        },
-        {
-            name: "jeans",
-            category: "bottom",
-            default_set: ["male", "female", "non"],
-            temp_range: {hi: 0, lo: 0},
-            priority: 1,
-            img: "https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/jeans.png?v=1642731834628"
-        },
-        {
-            name: "parka",
-            category: "top",
-            default_set: ["male", "female", "non"],
-            temp_range: {hi: 0, lo: 0},
-            priority: 1,
-            img: "https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/parka.png?v=1642731834675"
-        },
-        {
-            name: "shorts",
-            category: "bottom",
-            default_set: ["male", "female", "non"],
-            temp_range: {hi: 0, lo: 0},
-            priority: 1,
-            img: "https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/shorts.png?v=1642731834200"
-        },
-        {
-            name: "snow-pants",
-            category: "bottom",
-            default_set: ["male", "female", "non"],
-            temp_range: {hi: 0, lo: 0},
-            priority: 1,
-            img: "https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/snow-pants.png?v=1642731834707"
-        },
-        {
-            name: "sunglasses",
-            category: "accessory",
-            default_set: ["male", "female", "non"],
-            temp_range: {hi: 0, lo: 0},
-            priority: 1,
-            img: "https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/sunglasses.png?v=1642731834851"
-        },
-        {
-            name: "tank-top",
-            category: "top",
-            default_set: ["male", "female", "non"],
-            temp_range: {hi: 0, lo: 0},
-            priority: 1,
-            img: "https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/tank-top.png?v=1642731834978"
-        },
-        {
-            name: "tshirt",
-            category: "top",
-            default_set: ["male", "female", "non"],
-            temp_range: {hi: 0, lo: 0},
-            priority: 1,
-            img: "https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/tshirt.png?v=1642731835589"
-        },
-        {
-            name: "umbrella",
-            category: "accessory",
-            default_set: ["male", "female", "non"],
-            temp_range: {hi: 0, lo: 0},
-            priority: 1,
-            img: "https://cdn.glitch.global/03796fc2-c550-441d-9435-01df882dc421/umbrella.png?v=1642731835865"
-        }
-    ];
-
-
-const newClothing = {
-    name: "",
-    category: "",
-    temp_range: { hi: 0, lo: 0 },
-    priority: 1,
-    img: "",
-}
+db.
 
 module.exports = db
 
